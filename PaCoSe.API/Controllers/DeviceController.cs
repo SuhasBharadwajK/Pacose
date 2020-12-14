b6e86080463f16a229810a91f6a297bc5a923c11
Suhas Bharadwaj <suhas.five@gmail.com>
2020-12-15 01:37:15 +0530

    Added placeholder comments for API endpoints

---
+++ PaCoSe.API/Controllers/DeviceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaCoSe.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        // POST /authorize -> Anonymous
+
+        // GET /status -> Device Token
+
+        // POST /validate -> Device Token
+
+        // POST /add -> User Token
+
+        // POST /remove -> User Token
+
+        // POST /limits -> User Token
+    }
+}
---
+++ PaCoSe.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaCoSe.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // POST /add-owner/:deviceId -> User Token
+
+        // POST /add -> User Token
+
+        // GET / -> User Token
+
+        // GET /:id -> User Token
+
+        // DELETE /:id -> User Token
+
+        // PUT /:id -> User Token
+
+        // PUT /disable/:id -> User Token
+    }
+}
