33bd59625d1effc03d959c6922f937efabd95984
Suhas Bharadwaj <suhas.five@gmail.com>
2020-12-26 12:00:11 +0530

    Implemented toggling between dark and light themes

--- PaCoSe.Web/ClientApp/src/app/app.component.ts
+++ PaCoSe.Web/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isDarkThemeActive: boolean = false;
+
+  get currentThemeClass() {
+    return this.isDarkThemeActive ? 'app-theme-dark' : 'app-theme-light';
+  }
+
+  constructor(
+    private _overlayContainer: OverlayContainer
+  ) {
+    this.setAppTheme();
+  }
+
+  toggleTheme(isDarkThemeActive: boolean) {
+    this.isDarkThemeActive = isDarkThemeActive;
+    localStorage.setItem('isDarkThemeActive', this.isDarkThemeActive.toString());
+    this.setOverlayTheme();
+  }
+
+  setAppTheme() {
+    this.isDarkThemeActive = localStorage.getItem('isDarkThemeActive') ? localStorage.getItem('isDarkThemeActive') === 'true' : false;
+    this.setOverlayTheme();
+  }
+
+  setOverlayTheme() {
+    const overlayContainerClasses = this._overlayContainer.getContainerElement().classList;
+    const themeClassNames = Array.from(overlayContainerClasses)
+      .filter((item: string) => item.includes('app-theme-'));
+    if (themeClassNames.length) {
+      overlayContainerClasses.remove(...themeClassNames);
+    }
+
+    overlayContainerClasses.add(this.currentThemeClass);
+  }
 }
--- PaCoSe.Web/ClientApp/src/app/header/header.component.ts
+++ PaCoSe.Web/ClientApp/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { MatSidenav } from '@angular/material/sidenav';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-    @Input('drawer') drawer: MatSidenav;
+  @Input('drawer') drawer: MatSidenav;
+  @Input('isDarkThemeActive') isDarkThemeActive: boolean;
+  @Output('switchTheme') switchTheme: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    toggle() {
-      this.drawer.toggle();
+  constructor() {
+  }
+
+  toggle() {
+    this.drawer.toggle();
+  }
+
+  toggleTheme() {
+    this.switchTheme?.emit(!this.isDarkThemeActive);
   }
 }
